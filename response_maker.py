7e24e891d133373ed57b66135e4e4e075e3ec86b
Xujinh <xujinh-1@foxmail.com>
2023-10-21 16:17:21 +0800

    响应内容生成

--- response_maker.py
+++ response_maker.py
@@ -14,19 +14,20 @@ STATUS_CODE= {
     504: 'GATEWAY TIMEOUT'
 }
 
+PROTOCOL = 'HTTP/1.1'
+
 class ResponseMaker:
     global PROTOCOL
 
     def __init__(self,code=200):
         self.response_head = {
-            'Content-Type': 'text/html' ,
-            'charset': 'utf-8',  # Content-Type: text/html; charset=utf-8  要合在一起
+            'Content-Type': 'text/html; charset=utf-8',
             'Server': 'XHome',
             "Content-Encoding": "gzip",
             "Date": time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime()),
         }
-        self.response_body = ''
-        self.cange_cookie = {}
+        self.response_body = b''
+        self.cange_cookie = []
 
         self.first_line = PROTOCOL + ' ' + str(code) + ' ' + STATUS_CODE[code]
 
@@ -34,27 +35,77 @@ class ResponseMaker:
     def set_head(self,key,value):
         self.response_head[key] = value
 
-    def set_cookie(self,key,value):
+    def set_cookie(self,key,value=None,expires=None,path=None,domain=None,secure=None,httponly=False,samesite=None,max_age=None):
         """
         set_cookie关键字一条只能设置一个cookie
         注意拼接时key & value需要转换为字符串
         """
+        
+        cookie_setting = ""
+        if expires:
+            cookie_setting += "; Expires=" + expires
+        if path:
+            cookie_setting += "; Path=" + path
+        if domain:
+            cookie_setting += "; Domain=" + domain
+        if max_age:
+            cookie_setting += "; Max-Age=" + max_age
+        if samesite:
+            cookie_setting += "; SameSite=" + samesite
+        if secure:
+            cookie_setting += "; Secure"
+        if httponly:
+            cookie_setting += "; HttpOnly"
+        
+        
+        cookie_group = {}
+
         if type(key) == str:
-            self.cange_cookie[key] = value
-        elif type(key) == dict:
-            self.cange_cookie.update(key)
+            # key = 'key'
+            # value = 'value'
+            cookie_group[key] = value
+        elif type(key) == dict and value == None:
+            # key = {'key1':'value1','key2':'value2'}
+
+            cookie_group.update(key)
         elif type(key)==list & type(value)==list:
+            # key = [key1,key2,key3]
+            # value = [value1,value2,value3]
             if len(key) != len(value):
                 raise Exception('key和value长度不一致')
             for i in range(len(key)):
-                self.cange_cookie[key[i]] = value[i]
+                cookie_group[key[i]] = value[i]
+        elif type(key)==list & value==None:
+            # key = [[key,value],[key,value]]
+            for i in key:
+                cookie_group[i[0]] = i[1]
         else:
             raise Exception('set_cookie参数错误')
         
+        self.cange_cookie.append([cookie_group,cookie_setting])
+        
 
-    def set_body(self,body):
+    def set_body(self,body:bytes):
         self.response_body = body
 
-    def __str__(self):
+    def content(self):
         # TODO 未完成拼接response
-        return str(self.response_head) + '\r\n\r\n' + str(self.response_body)
+        
+        head = [str(i)+ ":" + str(j) for i,j in self.response_head.items() if j != None]
+
+        # head 按字母排序
+        head.sort()
+        head_str = '\r\n'.join(head)
+        head_str = self.first_line + '\r\n' + head_str
+
+        # cookie
+        cookie_str = ""
+        if len(self.cange_cookie) != 0:
+            for i in self.cange_cookie:
+                cookie_dict = i[0]
+                cookie_setting = i[1]
+                for key,value in cookie_dict.items():
+                    cookie_str += "\r\n" + "Set-Cookie: " + key + "=" + value + cookie_setting
+        
+        
+        return head_str.encode("utf-8") + cookie_str.encode("utf-8") + b'\r\n\r\n' + self.response_body
