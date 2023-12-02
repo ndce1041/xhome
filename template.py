4bc366ad292b86642ca94a928423fc1cdd5d972d
Xujinh <xujinh-1@foxmail.com>
2023-12-03 00:12:35 +0800

    添加简单模板功能

---
+++ template.py
@@ -0,0 +1,43 @@
+import re
+import response_maker as rm
+
+class Template:
+    def __init__(self,template_path,template_dict):
+        """
+        template_path: 模板路径
+        template_dict: 模板字典
+        基础的模板类
+        """
+        self.template_path = template_path
+        self.template_dict = template_dict
+        self.template = self.read_template()
+        self.replace_template()
+
+        return rm.ResponseMaker().set_body(self.template.encode("utf-8"))
+        
+    def read_template(self):
+        """
+        读取模板文件
+        """
+        with open(self.template_path,"rb") as f:
+            return f.read().decode("utf-8")
+        
+    def replace_template(self):
+        """
+        替换模板
+        """
+        self.template = re.sub(r"{{ (%s) }}",replace,self.template)
+        # return self.template
+
+        # comp = re.compile(r"{{ %s }}")
+
+        def replace(match):
+            temp = match.group(0)
+            temp = temp.split(".")
+            value = self.template_dict.copy()
+            for i in temp:
+                if i in value:
+                    value = value[i]
+                else:
+                    return "NOT FOUND"
+            return value
